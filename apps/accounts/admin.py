7c5e8052ed18ced35b7d29c1543df133e4cb5908
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-05-02 21:15:10 +0500

    Register Favorite and PlaybackHistory in Django admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User, Favorite, PlaybackHistory
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('email', 'first_name', 'last_name', 'avatar', 'bio')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'track_title', 'artist_name', 'created_at')
+    search_fields = ('user__username', 'track_title', 'artist_name')
+    list_filter = ('created_at',)
+
+
+@admin.register(PlaybackHistory)
+class PlaybackHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'track_title', 'artist_name', 'position', 'timestamp')
+    search_fields = ('user__username', 'track_title', 'artist_name')
+    list_filter = ('timestamp',)
