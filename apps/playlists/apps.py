e4f7b40e83b47417419fff5272eb66fa21b82d73
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-05-02 21:15:13 +0500

    Define PlaylistsConfig in apps/playlists/apps.py

--- apps/playlists/apps.py
+++ apps/playlists/apps.py
@@ -3,4 +3,10 @@ from django.apps import AppConfig
 
 class PlaylistsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'playlists'
+    name = 'apps.playlists'
+
+    def ready(self):
+        try:
+            import apps.playlists.signals
+        except ImportError:
+            pass
