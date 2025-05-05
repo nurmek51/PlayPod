2c620dbf67a117084d576c1fc9fe499376d88669
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-05-06 04:30:23 +0500

    chore: Update project configuration settings
    - Adjust settings for development environment
    - Update authentication settings to fix token endpoint

--- PlayPod/settings.py
+++ PlayPod/settings.py
@@ -111,7 +111,8 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:5173",    "http://localhost:8080",
+    "http://127.0.0.1:8080",]
 CORS_ALLOW_CREDENTIALS = True
 
 REDIS_HOST = os.getenv("REDIS_HOST")
